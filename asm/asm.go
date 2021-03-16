78268503a1410d25690eb143c6d4164a975dd9d1
Stephen Fox <stephen.j.fox.jr@gmail.com>
2021-03-16 01:31:03 -0400

    Abstract assembly decoding.

--- asm/asm.go
+++ asm/asm.go
@@ -6,6 +6,147 @@ import (
 	"golang.org/x/arch/x86/x86asm"
 )
 
+const (
+	SkipSyntax  DisassemblySyntax = ""
+	ATTSyntax   DisassemblySyntax = "att"
+	GoSyntax    DisassemblySyntax = "go"
+	IntelSyntax DisassemblySyntax = "intel"
+)
+
+type DisassemblySyntax string
+
+type DecoderConfig struct {
+	Disassemble DisassemblySyntax
+	ArchConfig  interface{}
+}
+
+type X86Config struct {
+	Bits int
+}
+
+type ARMConfig struct {
+	Mode armasm.Mode
+}
+
+func NewDecoder(config DecoderConfig) (*Decoder, error) {
+	switch assertedConfig := config.ArchConfig.(type) {
+	case ARMConfig:
+		var dissassemFn func(inst armasm.Inst) string
+		switch config.Disassemble {
+		case SkipSyntax:
+			// Do nothing.
+		case ATTSyntax:
+			dissassemFn = armasm.GNUSyntax
+		default:
+			return nil, fmt.Errorf("unsupported syntax type for arm: %s", config.Disassemble)
+		}
+
+		return &Decoder{
+			decodeOneInstFn: func(singleInst []byte) (Inst, error) {
+				armInst, err := armasm.Decode(singleInst, assertedConfig.Mode)
+				if err != nil {
+					return Inst{}, err
+				}
+
+				var disassembly string
+				if dissassemFn != nil {
+					disassembly = dissassemFn(armInst)
+				}
+
+				return Inst{
+					Raw:  singleInst,
+					Hex:  fmt.Sprintf("0x%x", singleInst[0:armInst.Len]),
+					Len:  armInst.Len,
+					Dis:  disassembly,
+					Inst: armInst,
+				}, nil
+			},
+		}, nil
+	case X86Config:
+		var disassemblyFn func(inst x86asm.Inst) string
+		switch config.Disassemble {
+		case SkipSyntax:
+			// Do nothing.
+		case ATTSyntax:
+			disassemblyFn = func(inst x86asm.Inst) string {
+				return x86asm.GNUSyntax(inst, 0, nil)
+			}
+		case GoSyntax:
+			disassemblyFn = func(inst x86asm.Inst) string {
+				return x86asm.GoSyntax(inst, 0, nil)
+			}
+		case IntelSyntax:
+			disassemblyFn = func(inst x86asm.Inst) string {
+				return x86asm.IntelSyntax(inst, 0, nil)
+			}
+		default:
+			return nil, fmt.Errorf("unsupported syntax type for x86: %s", config.Disassemble)
+		}
+
+		return &Decoder{
+			decodeOneInstFn: func(singleInst []byte) (Inst, error) {
+				x86Inst, err := x86asm.Decode(singleInst, assertedConfig.Bits)
+				if err != nil {
+					return Inst{}, err
+				}
+
+				var disassembly string
+				if disassemblyFn != nil {
+					disassembly = disassemblyFn(x86Inst)
+				}
+
+				return Inst{
+					Raw:  singleInst,
+					Hex:  fmt.Sprintf("0x%x", singleInst[0:x86Inst.Len]),
+					Len:  x86Inst.Len,
+					Dis:  disassembly,
+					Inst: x86Inst,
+				}, nil
+			},
+		}, nil
+	default:
+		return nil, fmt.Errorf("unsupported config type: %T", assertedConfig)
+	}
+}
+
+type Decoder struct {
+	decodeOneInstFn func(rawInstructions []byte) (Inst, error)
+}
+
+func (o Decoder) DecodeAll(rawInstructions []byte, onDecodeFn func(Inst)) error {
+	index := 0
+	for {
+		if isDone(rawInstructions, index) {
+			return nil
+		}
+
+		inst, err := o.decodeOneInstFn(rawInstructions[index:])
+		if err != nil {
+			return fmt.Errorf("failed to decode instruction %d - %w - remaining data: 0x%x",
+				index, err, rawInstructions[index:])
+		}
+
+		inst.Index = index
+
+		onDecodeFn(inst)
+
+		index += inst.Len
+	}
+}
+
+func (o Decoder) DecodeFirst(rawInstructions []byte) (Inst, error) {
+	return o.decodeOneInstFn(rawInstructions)
+}
+
+type Inst struct {
+	Raw   []byte
+	Hex   string
+	Len   int
+	Index int
+	Dis   string
+	Inst  interface{}
+}
+
 func DecodeX86(rawInstructions []byte, bits int, onDecodeFn func(inst x86asm.Inst, index int)) error {
 	index := 0
 	for {
--- cmd/foo/main.go
+++ cmd/foo/main.go
@@ -5,15 +5,14 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"gitlab.com/stephen-fox/brkit/asm"
+	"gitlab.com/stephen-fox/brkit/conv"
 	"golang.org/x/arch/arm/armasm"
 	"golang.org/x/arch/x86/x86asm"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
-
-	"gitlab.com/stephen-fox/brkit/asm"
-	"gitlab.com/stephen-fox/brkit/conv"
 )
 
 const (
@@ -57,6 +56,113 @@ options:
 )
 
 func main() {
+	inputFormat := flag.String(
+		inputFormatArg,
+		hexFormat,
+		"The input data format")
+	outputFormat := flag.String(
+		outputFormatArg,
+		prettyFormat,
+		"")
+	syntax := flag.String(
+		asmSyntaxArg,
+		intelSyntax,
+		"The desired assembly syntax")
+	help := flag.Bool(
+		helpArg,
+		false,
+		"Display this help page")
+
+	flag.Parse()
+
+	if *help {
+		os.Stderr.WriteString(usage)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if flag.NArg() != 1 {
+		log.Fatalf("please specify a platform for decode for ('%s', '%s', '%s')",
+			armPlatform, x86_32Platform, x86_32Platform)
+	}
+
+	config := asm.DecoderConfig{
+		Disassemble: asm.DisassemblySyntax(*syntax),
+	}
+	platform := flag.Arg(0)
+	switch platform {
+	case armPlatform:
+		config.ArchConfig = asm.ARMConfig{Mode: armasm.ModeARM}
+	case x86_32Platform, x86_64Platform:
+		bits := 32
+		if platform == x86_64Platform {
+			bits = 64
+		}
+
+		config.ArchConfig = asm.X86Config{Bits: bits}
+	default:
+		log.Fatalf("unsupported platform: '%s'", platform)
+	}
+
+	decoder, err := asm.NewDecoder(config)
+	if err != nil {
+		log.Fatalf("failed to create new decoder for - %s", err)
+	}
+
+	var decoded []byte
+	switch *inputFormat {
+	case b64Format:
+		b64Str, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalf("failed to read base64 data from stdin - %s", err)
+		}
+		decoded = make([]byte, base64.StdEncoding.DecodedLen(len(b64Str)))
+		_, err = base64.StdEncoding.Decode(decoded, b64Str)
+	case hexFormat:
+		decoded, err = conv.HexArrayToBytes(os.Stdin)
+	case rawFormat:
+		decoded, err = ioutil.ReadAll(os.Stdin)
+	default:
+		log.Fatalf("unknown data format: %s", *inputFormat)
+	}
+	if err != nil {
+		log.Fatalf("failed to read %s data - %s", *inputFormat, err)
+	}
+
+	var jsonOut []string
+	err = decoder.DecodeAll(decoded, func(inst asm.Inst) {
+		switch *outputFormat {
+		case prettyFormat:
+			fmt.Println(inst.Dis)
+		case jsonVerboseFormat:
+			raw, err := json.MarshalIndent(&inst, "", "    ")
+			if err != nil {
+				log.Fatalf("failed to marshal instruction to json - %s", err)
+			}
+			jsonOut = append(jsonOut, string(raw))
+		case jsonHumanFormat:
+			jsonOut = append(jsonOut, inst.Dis)
+		}
+	})
+	if err != nil {
+		log.Fatalf("failed to decode instructions for '%s' - %s", platform, err)
+	}
+
+	if len(jsonOut) > 0 {
+		switch *outputFormat {
+		case jsonHumanFormat:
+			raw, err := json.MarshalIndent(jsonOut, "", "    ")
+			if err != nil {
+				log.Fatalf("failed to marshal instructions summary slice to json - %s", err)
+			}
+			fmt.Printf("%s\n", raw)
+		case jsonVerboseFormat:
+			fmt.Printf("{[%s]}\n", strings.Join(jsonOut, ",\n"))
+		}
+	}
+}
+
+func main2() {
 	inputFormat := flag.String(
 		inputFormatArg,
 		hexFormat,
