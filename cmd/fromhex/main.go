c6c49e39b40ad45fdbb87aaa7833bebe6e6ce769
Stephen Fox <stephen.j.fox.jr@gmail.com>
2023-07-17 07:42:33 -0400

    fromhex: Go []byte output format.

--- cmd/fromhex/main.go
+++ cmd/fromhex/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/base64"
 	"flag"
@@ -17,11 +18,13 @@ import (
 
 const (
 	outputFormatArg = "o"
+	noFmtArg        = "n"
 	helpArg         = "h"
 
 	hexFormat = "hex"
 	rawFormat = "raw"
 	b64Format = "b64"
+	goFormat  = "go"
 
 	appName = "fromhex"
 	usage   = appName + `
@@ -51,7 +54,11 @@ func main() {
 	outputEncoding := flag.String(
 		outputFormatArg,
 		rawFormat,
-		fmt.Sprintf("The output encoding type (%s)", supportedIOEncodingStr()))
+		fmt.Sprintf("The output encoding type\n(%s)", supportedIOEncodingStr()))
+	noFormatting := flag.Bool(
+		noFmtArg,
+		false,
+		"Do not format output data (i.e., apply spacing or newlines)")
 	help := flag.Bool(
 		helpArg,
 		false,
@@ -64,6 +71,8 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+
+	log.SetFlags(0)
 
 	var sourceName string
 	var scannerSource io.Reader
@@ -96,11 +105,49 @@ func main() {
 		fmt.Printf("%s", decoded)
 	case b64Format:
 		fmt.Print(base64.StdEncoding.EncodeToString(decoded))
+	case goFormat:
+		decodedLen := len(decoded)
+		currentLineLen := 0
+		bufioWriter := bufio.NewWriter(os.Stdout)
+
+		_, err := bufioWriter.WriteString("[]byte{")
+		if err != nil {
+			log.Fatalf("fatal: %s", err)
+		}
+
+		for i, b := range decoded {
+			needsComma := decodedLen > 1 && i != decodedLen-1
+
+			bufioWriter.WriteString("0x")
+			bufioWriter.WriteString(fmt.Sprintf("%x", b))
+			currentLineLen += 4
+
+			if needsComma {
+				bufioWriter.WriteByte(',')
+				currentLineLen++
+			}
+
+			if !*noFormatting && currentLineLen >= 62 {
+				currentLineLen = 0
+				bufioWriter.WriteString("\n\t")
+			} else if needsComma {
+				bufioWriter.WriteByte(' ')
+				currentLineLen++
+			}
+		}
+
+		bufioWriter.WriteString("}\n")
+
+		err = bufioWriter.Flush()
+		if err != nil {
+			log.Fatalf("fatal: %s", err)
+		}
 	default:
 		log.Fatalf("unknown output format: '%s'", *outputEncoding)
 	}
 }
 
 func supportedIOEncodingStr() string {
-	return fmt.Sprintf("'%s', '%s', '%s'", b64Format, hexFormat, rawFormat)
+	return fmt.Sprintf("'%s', '%s', '%s', '%s'",
+		b64Format, hexFormat, goFormat, rawFormat)
 }
