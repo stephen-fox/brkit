849e18a22cc754f287df0fa3f5bb66f88091425b
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-04-12 20:46:59 -0400

    asmkit: Support streaming input in Disassembler

--- asmkit/asmkit.go
+++ asmkit/asmkit.go
@@ -1,7 +1,9 @@
 package asmkit
 
 import (
+	"errors"
 	"fmt"
+	"io"
 
 	"golang.org/x/arch/arm/armasm"
 	"golang.org/x/arch/x86/x86asm"
@@ -17,6 +19,7 @@ const (
 type DisassemblySyntax string
 
 type DisassemblerConfig struct {
+	Src        io.Reader
 	Syntax     DisassemblySyntax
 	ArchConfig interface{}
 }
@@ -25,13 +28,34 @@ type X86Config struct {
 	Bits int
 }
 
-type ARMConfig struct {
-	Mode armasm.Mode
+type ArmConfig struct {
+	Mode ArmMode
 }
 
+// A Mode is an instruction execution mode.
+//
+// This type allows callers to interoperate with golang.org/x/arch/x86/x86asm
+// without forcing them to rely on it directly.
+type ArmMode int
+
+func (o ArmMode) toArmasmMode() armasm.Mode {
+	return armasm.Mode(o)
+}
+
+const (
+	ModeARM   = ArmMode(armasm.ModeARM)
+	ModeThumb = ArmMode(armasm.ModeThumb)
+)
+
 func NewDisassembler(config DisassemblerConfig) (*Disassembler, error) {
+	if config.Src == nil {
+		return nil, errors.New("source reader is nil")
+	}
+
+	reader := config.Src
+
 	switch assertedConfig := config.ArchConfig.(type) {
-	case ARMConfig:
+	case ArmConfig:
 		var dissassemFn func(inst armasm.Inst) string
 		switch config.Syntax {
 		case SkipSyntax:
@@ -39,12 +63,13 @@ func NewDisassembler(config DisassemblerConfig) (*Disassembler, error) {
 		case ATTSyntax:
 			dissassemFn = armasm.GNUSyntax
 		default:
-			return nil, fmt.Errorf("unsupported syntax type for arm: %s", config.Syntax)
+			return nil, fmt.Errorf("unsupported syntax type for arm: %q", config.Syntax)
 		}
 
 		return &Disassembler{
+			reader: reader,
 			disassOneInstFn: func(remainingInsts []byte) (Inst, error) {
-				armInst, err := armasm.Decode(remainingInsts, assertedConfig.Mode)
+				armInst, err := armasm.Decode(remainingInsts, assertedConfig.Mode.toArmasmMode())
 				if err != nil {
 					return Inst{}, err
 				}
@@ -86,6 +111,7 @@ func NewDisassembler(config DisassemblerConfig) (*Disassembler, error) {
 		}
 
 		return &Disassembler{
+			reader: reader,
 			disassOneInstFn: func(remainingInsts []byte) (Inst, error) {
 				x86Inst, err := x86asm.Decode(remainingInsts, assertedConfig.Bits)
 				if err != nil {
@@ -121,37 +147,121 @@ func copySlice(src []byte, numBytes int) []byte {
 }
 
 type Disassembler struct {
+	reader          io.Reader
 	disassOneInstFn func(remainingInsts []byte) (Inst, error)
+	last            Inst
+	buf             []byte
+	readerDone      bool
+	index           int
+	isDone          bool
+	err             error
 }
 
-func (o *Disassembler) All(rawInstructions []byte, onDecodeFn func(Inst) error) error {
-	index := 0
-
-	for {
-		if isDone(rawInstructions, index) {
-			return nil
-		}
-
-		inst, err := o.disassOneInstFn(rawInstructions[index:])
+func (o *Disassembler) All(onDecodeFn func(Inst) error) error {
+	for o.Next() {
+		err := onDecodeFn(o.last)
 		if err != nil {
-			return fmt.Errorf("failed to decode instruction %d - %w - remaining data: 0x%x",
-				index, err, rawInstructions[index:])
+			return fmt.Errorf("instruction %d - on decode function failed (%q) - %w",
+				o.index, o.last.Dis, err)
 		}
-
-		inst.Index = index
-
-		err = onDecodeFn(inst)
-		if err != nil {
-			return fmt.Errorf("on decode function failed for instruction %d (%q) - %w",
-				index, inst.Dis, err)
-		}
-
-		index += inst.Len
 	}
+
+	err := o.Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (o *Disassembler) Next(rawInstructions []byte) (Inst, error) {
-	return o.disassOneInstFn(rawInstructions)
+func (o *Disassembler) Err() error {
+	if o.err != nil {
+		return o.err
+	}
+
+	return nil
+}
+
+func (o *Disassembler) Inst() Inst {
+	return o.last
+}
+
+func (o *Disassembler) Next() bool {
+	return o.next()
+}
+
+func (o *Disassembler) next() bool {
+	if o.err != nil || o.isDone {
+		return false
+	}
+
+	inst, hasMore, err := o.parseNext()
+	if err != nil {
+		o.err = fmt.Errorf("instruction %d - %w",
+			o.index, err)
+
+		return false
+	}
+
+	o.last = inst
+
+	if hasMore {
+		return true
+	}
+
+	o.isDone = true
+
+	return false
+}
+
+func (o *Disassembler) parseNext() (Inst, bool, error) {
+	err := o.read()
+	if err != nil {
+		return Inst{}, false, err
+	}
+
+	if len(o.buf) == 0 {
+		return Inst{}, false, nil
+	}
+
+	inst, err := o.disassOneInstFn(o.buf)
+	if err != nil {
+		return Inst{}, false, fmt.Errorf("disassembly failed - %w", err)
+	}
+
+	o.buf = o.buf[inst.Len:]
+
+	o.index++
+
+	return inst, len(o.buf) > 0, nil
+}
+
+func (o *Disassembler) read() error {
+	if o.readerDone {
+		return nil
+	}
+
+	const readSizeBytes = 1024
+
+	if len(o.buf) < readSizeBytes {
+		b := make([]byte, readSizeBytes)
+
+		n, err := o.reader.Read(b)
+		switch {
+		case err == nil:
+			o.buf = append(o.buf, b[0:n]...)
+		case errors.Is(err, io.EOF):
+			o.buf = append(o.buf, b[0:n]...)
+
+			o.readerDone = true
+		default:
+			o.readerDone = true
+
+			return err
+		}
+	}
+
+	return nil
 }
 
 type Inst struct {
