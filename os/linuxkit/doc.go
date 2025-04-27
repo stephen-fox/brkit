182d387c53fd91e8d3b77437cc1070b1688736a0
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-04-27 20:49:14 +0000

    linuxkit: Initial commit

---
+++ os/linuxkit/doc.go
@@ -0,0 +1,3 @@
+// Package linuxkit provides functionality for working with Linux-speciifc
+// software in exploitation.
+package linuxkit
---
+++ os/linuxkit/sigcontext.go
@@ -0,0 +1,47 @@
+package linuxkit
+
+// Sigcontext is the struct that is read by sigreturn(2).
+// It is useful for manipulating CPU registers using SROP
+// (Sigreturn-oriented programming).
+type Sigcontext struct {
+	IDK1    uint64
+	IDK2    uint64
+	IDK3    uint64
+	IDK4    uint64
+	IDK5    uint64
+	R8      uint64
+	R9      uint64
+	R10     uint64
+	R11     uint64
+	R12     uint64
+	R13     uint64
+	R14     uint64
+	R15     uint64
+	RDI     uint64
+	RSI     uint64
+	RBP     uint64
+	RBX     uint64
+	RDX     uint64
+	RAX     uint64
+	RCX     uint64
+	RSP     uint64
+	RIP     uint64
+	EFLAGS  uint64
+	CS      uint16
+	GS      uint16
+	FS      uint16
+	PAD0    uint16
+	ERR     uint64
+	TRAPNO  uint64
+	OLDMASK uint64
+	CR2     uint64
+	Fpstate uint64
+	RES0    uint64
+	RES1    uint64
+	RES2    uint64
+	RES3    uint64
+	RES4    uint64
+	RES5    uint64
+	RES6    uint64
+	RES7    uint64
+}
