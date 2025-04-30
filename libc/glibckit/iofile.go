d3e1af5486dc8efade571748bd6d90057aae10d1
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-04-30 00:41:18 +0000

    glibckit: Fixed IOFile visibility

--- libc/glibckit/iofile.go
+++ libc/glibckit/iofile.go
@@ -1,40 +1,40 @@
 package glibckit
 
-// _IO_FILE is used by glibc to store state for files.
+// IO_FILE is used by glibc to store state for files.
 // This struct is useful for implementing FSOP (file
 // stream oriented programming).
 //
 // This struct was generated using glibc 2.31.
-type _IO_FILE struct {
-	_flags          uint64
-	_IO_read_ptr    uint64 // *byte
-	_IO_read_end    uint64 // *byte
-	_IO_read_base   uint64 // *byte
-	_IO_write_base  uint64 // *byte
-	_IO_write_ptr   uint64 // *byte
-	_IO_write_end   uint64 // *byte
-	_IO_buf_base    uint64 // *byte
-	_IO_buf_end     uint64 // *byte
-	_IO_save_base   uint64 // *byte
-	_IO_backup_base uint64 // *byte
-	_IO_save_end    uint64 // *byte
-	_markers        uint64 // *_IO_marker
-	_chain          uint64 // *_IO_FILE
-	_fileno         int32
-	_flags2         int32
-	_old_offset     uint64 // __off_t
-	_cur_column     uint16
-	_vtable_offset  int8
-	_shortbuf       [1]byte
-	unknown1        int32
-	_lock           uint64 // unsafe.Pointer
-	_offset         uint64 // __off64_t
-	_codecvt        uint64 // *_IO_codecvt
-	_wide_data      uint64 // *_IO_wide_data
+type IO_FILE struct {
+	Flags          uint64
+	IO_read_ptr    uint64 // *byte
+	IO_read_end    uint64 // *byte
+	IO_read_base   uint64 // *byte
+	IO_write_base  uint64 // *byte
+	IO_write_ptr   uint64 // *byte
+	IO_write_end   uint64 // *byte
+	IO_buf_base    uint64 // *byte
+	IO_buf_end     uint64 // *byte
+	IO_save_base   uint64 // *byte
+	IO_backup_base uint64 // *byte
+	IO_save_end    uint64 // *byte
+	Markers        uint64 // *_IO_marker
+	Chain          uint64 // *_IO_FILE
+	Fileno         int32
+	Flags2         int32
+	Old_offset     uint64 // __off_t
+	Cur_column     uint16
+	Vtable_offset  int8
+	Shortbuf       [1]byte
+	Unknown1       int32
+	Lock           uint64 // unsafe.Pointer
+	Offset         uint64 // __off64_t
+	Codecvt        uint64 // *_IO_codecvt
+	Wide_data      uint64 // *_IO_wide_data
 	// Unclear if these fields are needed.
-	//_freeres_list   uint64 // *_IO_FILE
-	//_freeres_buf    uint64 // unsafe.Pointer
-	//__pad5          uint64 // size_t
-	//_mode           int32
-	_unused2 [80]byte
+	//Freeres_list uint64 // *_IO_FILE
+	//Freeres_buf  uint64 // unsafe.Pointer
+	//Pad5         uint64 // size_t
+	//Mode         int32
+	Unused2 [80]byte
 }
