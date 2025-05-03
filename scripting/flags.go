bf40eaad5890f762ed8f83004895324eccd7a108
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-05-03 20:25:51 +0000

    scripting: Make ParseExploitArgs more usable

--- scripting/flags.go
+++ scripting/flags.go
@@ -1,12 +1,15 @@
 package scripting
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"gitlab.com/stephen-fox/brkit/process"
 )
@@ -17,7 +20,11 @@ type ParseExploitArgsConfig struct {
 	OptLogger  *log.Logger
 }
 
-func ParseExploitArgs(config ParseExploitArgsConfig) *process.Process {
+func ParseExploitArgs(config ParseExploitArgsConfig) (*process.Process, ExploitArgs) {
+	return ParseExploitArgsCtx(context.Background(), config)
+}
+
+func ParseExploitArgsCtx(ctx context.Context, config ParseExploitArgsConfig) (*process.Process, ExploitArgs) {
 	logger := log.Default()
 	if config.OptLogger != nil {
 		logger = config.OptLogger
@@ -27,53 +34,109 @@ func ParseExploitArgs(config ParseExploitArgsConfig) *process.Process {
 		logger.SetFlags(0)
 	}
 
-	proc, err := parseExploitArgs(config)
+	proc, args, err := parseExploitArgs(ctx, logger, config)
 	if err != nil {
 		logger.Fatalln("fatal:", err)
 	}
 
-	return proc
+	return proc, args
 }
 
-const usage = `please specify one of the following:
-  local EXE-PATH
-  ssh SSH-SERVER-ADDRESS STD-PIPES-DIR-PATH
-  remote ADDRESS`
-
 type ExploitArgs struct {
-	Process     *process.Process
-	Help        bool
 	StageNumber int
 	Verbose     *log.Logger
 }
 
 type tempExploitArgs struct {
-	Help        bool
-	StageNumber int
-	Verbose     bool
+	help              bool
+	stageNumber       int
+	verboseLogging    bool
+	enableProcLogging bool
 }
 
-func parseExploitArgs(config ParseExploitArgsConfig) (*process.Process, error) {
-	var exploitArgs tempExploitArgs
+func (o tempExploitArgs) toExploitArgs(logger *log.Logger) ExploitArgs {
+	args := ExploitArgs{
+		StageNumber: o.stageNumber,
+		Verbose:     log.New(io.Discard, "", 0),
+	}
+
+	if o.verboseLogging {
+		args.Verbose = log.New(logger.Writer(), "[!] ", logger.Flags())
+	}
+
+	return args
+}
+
+func parseExploitArgs(ctx context.Context, logger *log.Logger, config ParseExploitArgsConfig) (*process.Process, ExploitArgs, error) {
+	var tempArgs tempExploitArgs
 
 	flagSet := flag.CommandLine
+
 	if config.OptFlagSet != nil {
 		flagSet = config.OptFlagSet
 	} else {
-		flagSet.BoolVar(&exploitArgs.Help, "h", false, "display this information")
+		flagSet.BoolVar(
+			&tempArgs.help,
+			"h",
+			false,
+			"Display this information")
+
+		flagSet.BoolVar(
+			&tempArgs.enableProcLogging,
+			"V",
+			false,
+			"Log all process input and output")
+
+		flagSet.BoolVar(
+			&tempArgs.verboseLogging,
+			"v",
+			false,
+			"Enable verbose logging")
+
+		flagSet.IntVar(
+			&tempArgs.stageNumber,
+			"s",
+			0,
+			"Pause execution at the specified stage number")
 	}
 
 	if !flagSet.Parsed() {
 		err := flagSet.Parse(os.Args[1:])
 		if err != nil {
-			return nil, err
+			return nil, ExploitArgs{}, err
 		}
 	}
 
-	if flagSet.NArg() == 0 {
-		return nil, errors.New(usage)
+	if tempArgs.help {
+		name := filepath.Base(os.Args[0])
+
+		flagSet.Output().Write([]byte(`DESCRIPTION
+  Exploit ` + name + `.
+
+USAGE
+  ` + name + ` [options] local EXE-PATH
+  ` + name + ` [options] ssh SSH-SERVER-ADDRESS STD-PIPES-DIR-PATH
+  ` + name + ` [options] remote ADDRESS
+
+OPTIONS
+`))
+
+		flagSet.PrintDefaults()
+
+		os.Exit(1)
+
+		return nil, ExploitArgs{}, errors.New("unreachable")
 	}
 
+	if flagSet.NArg() == 0 {
+		return nil, ExploitArgs{}, errors.New(`please specify one of the following:
+  local EXE-PATH
+  ssh SSH-SERVER-ADDRESS STD-PIPES-DIR-PATH
+  remote ADDRESS`)
+	}
+
+	var proc *process.Process
+	var err error
 	mode := flagSet.Arg(0)
 
 	switch mode {
@@ -81,7 +144,7 @@ func parseExploitArgs(config ParseExploitArgsConfig) (*process.Process, error) {
 		exePath := flagSet.Arg(1)
 
 		if exePath == "" {
-			return nil, errors.New("please specify the local executable path as the last argument")
+			return nil, ExploitArgs{}, errors.New("please specify the local executable path as the last argument")
 		}
 
 		var additionalArgs []string
@@ -89,41 +152,56 @@ func parseExploitArgs(config ParseExploitArgsConfig) (*process.Process, error) {
 			additionalArgs = flagSet.Args()[2:]
 		}
 
-		return process.ExecOrExit(
-			exec.Command(exePath, additionalArgs...),
-			config.ProcInfo), nil
+		proc, err = process.Exec(
+			exec.CommandContext(ctx, exePath, additionalArgs...),
+			config.ProcInfo)
+		if err != nil {
+			return nil, ExploitArgs{}, fmt.Errorf("failed to exec start process - %w", err)
+		}
 	case "ssh":
 		addr := flagSet.Arg(1)
 		if addr == "" {
-			return nil, errors.New("please specify the ssh server address to connect to as the first non-flag argument")
+			return nil, ExploitArgs{}, errors.New("please specify the ssh server address to connect to as the first non-flag argument")
 		}
 
 		pipesDirPath := flagSet.Arg(2)
 		if pipesDirPath == "" {
-			return nil, errors.New("please specify the directory path containing the stdin and stdout pipe files as the second non-flag argument")
+			return nil, ExploitArgs{}, errors.New("please specify the directory path containing the stdin and stdout pipe files as the second non-flag argument")
 		}
 
-		sshInput := process.ExecOrExit(exec.Command(
+		sshInput := process.ExecOrExit(exec.CommandContext(
+			ctx,
 			"ssh", addr,
 			"--",
 			"cat", ">", pipesDirPath+"/stdin"),
 			process.X86_64Info())
 
-		sshOutput := process.ExecOrExit(exec.Command(
+		sshOutput := process.ExecOrExit(exec.CommandContext(
+			ctx,
 			"ssh", addr,
 			"--",
 			"cat", pipesDirPath+"/stdout"),
 			process.X86_64Info())
 
-		return process.FromIO(sshInput, sshOutput, process.X86_64Info()), nil
+		proc = process.FromIOCtx(ctx, sshInput, sshOutput, config.ProcInfo)
 	case "remote":
 		addr := flagSet.Arg(1)
 		if addr == "" {
-			return nil, errors.New("please specify the remote address as the last non-flag argument")
+			return nil, ExploitArgs{}, errors.New("please specify the remote address as the last non-flag argument")
 		}
 
-		return process.DialOrExit("tcp", addr, config.ProcInfo), nil
+		proc, err = process.DialCtx(ctx, "tcp", addr, config.ProcInfo)
+		if err != nil {
+			return nil, ExploitArgs{}, fmt.Errorf("failed to dial target - %w", err)
+		}
 	default:
-		return nil, fmt.Errorf("unknown mode: %q - %s", mode, usage)
+		return nil, ExploitArgs{}, fmt.Errorf("unknown mode: %q", mode)
 	}
+
+	if tempArgs.enableProcLogging {
+		proc.SetLoggerR(log.New(logger.Writer(), "<", logger.Flags()))
+		proc.SetLoggerW(log.New(logger.Writer(), ">", logger.Flags()))
+	}
+
+	return proc, tempArgs.toExploitArgs(logger), nil
 }
